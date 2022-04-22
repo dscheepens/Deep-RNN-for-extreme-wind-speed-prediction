385253fb0aa11c38932c6ad2f22f5103b567e030
Daan Scheepens <scheepensd94dm@leia.dm.univie.ac.at>
2022-04-22 16:55:18 +0200

    added grib_to_numpy.py

---
+++ grib_to_numpy.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pygrib 
+
+
+# Data from: 
+# https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-pressure-levels?tab=form
+# 'U-component of wind' and 'V-component of wind' at 1000, 925, 850 or 775 hPa. 
+# Files were renamed: adaptor.mars.internal-U-component_[insert hPa here].grib
+
+
+root = '../../../../../mnt/data/scheepensd94dm/data/'
+
+for hpa in [1000, 925, 850, 775]:
+    arr = [] 
+    
+    # Merge U and V velocities
+    grbs_u = pygrib.open(root+'adaptor.mars.internal-U-component_%s.grib'%hpa)
+    grbs_v = pygrib.open(root+'adaptor.mars.internal-V-component_%s.grib'%hpa)
+
+    for grb_u, grb_v in zip(grbs_u, grbs_v):
+        values = np.sqrt(grb_u.values**2 + grb_v.values**2)
+        arr.append(values)
+        
+    np.save(root+'adaptor.mars.internal-Horizontal_velocity_%s.npy'%hpa, np.array(arr))
